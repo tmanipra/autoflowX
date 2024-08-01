47c2a010344a39c3e63f7bc8f91fe45643b92286
tmanipra <53463040+tmanipra@users.noreply.github.com>
2024-08-01 12:18:10 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,63 @@ provider "google" {
   region  = var.location
 }
 
+resource "google_storage_bucket_object" "function_code" {
+  name   = "function.zip"
+  bucket = google_storage_bucket.util_bucket.name
+  source = data.archive_file.function_code.output_path
+}
+
+
+resource "google_cloudfunctions2_function" "function" {
+  name        = "gcs-to-gcs-function"
+  location    = var.region
+  description = "A function triggered by GCS"
+
+  build_config {
+    runtime     = "python312"
+    entry_point = "gcs_to_gcs"
+    environment_variables = {
+      PROJECT_ID = var.project_id
+
+    }
+    source {
+      storage_source {
+        bucket = google_storage_bucket.util_bucket.name
+        object = google_storage_bucket_object.function_code.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count = 3
+    min_instance_count = 0
+    available_memory   = "256M"
+    timeout_seconds    = 60
+
+    environment_variables = {
+      PROJECT_ID = var.project_id
+
+    }
+
+    ingress_settings               = "ALLOW_INTERNAL_ONLY"
+    all_traffic_on_latest_revision = true
+    service_account_email          = google_service_account.function_service_account.email
+  }
+
+  event_trigger {
+    event_type            = "google.cloud.storage.object.v1.finalized"
+    retry_policy          = "RETRY_POLICY_RETRY"
+    service_account_email = google_service_account.function_service_account.email
+    event_filters {
+      attribute = "bucket"
+      value     = google_storage_bucket.source_bucket.name
+    }
+  }
+
+  depends_on = [google_service_account.service_account,
+  google_project_iam_member.member-role]
+}
+
 resource "google_storage_bucket" "source_bucket" {
   name          = var.source_bucket_name
   location      = var.location
