235b1a1a3663d3be4d5a7fa957193e7d012a992b
tmanipra <53463040+tmanipra@users.noreply.github.com>
2024-08-01 17:03:21 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,21 +1,31 @@
+import functions_framework
 import os
 from google.cloud import storage
 
-def gcs_to_gcs(event, context):
-    bucket_name = event['bucket']
-    file_name = event['name']
+# Triggered by a change in a storage bucket
+@functions_framework.cloud_event
+def gcs_to_gcs(cloud_event):
+    # Get event data
+    data = cloud_event.data
+    bucket_name = data.get("bucket")
+    file_name = data.get("name")
 
-    if not file_name.endswith('.csv'):
-        return
-
+    # Initialize the Google Cloud Storage client
     client = storage.Client()
+
+    # Get the source and destination buckets
     source_bucket = client.bucket(bucket_name)
-    destination_bucket_name = os.environ['DEST_BUCKET']
+    destination_bucket_name = 'autoflowx_dest'
     destination_bucket = client.bucket(destination_bucket_name)
-    blob = source_bucket.blob(file_name)
 
-    # Copy the file to the destination bucket
-    source_bucket.copy_blob(blob, destination_bucket, file_name)
+    # Get the source blob and destination blob
+    source_blob = source_bucket.blob(file_name)
+    destination_blob = destination_bucket.blob(file_name)
 
-    # Optionally delete the file from the source bucket
-    blob.delete()
+    # Copy the blob to the destination bucket
+    destination_bucket.copy_blob(source_blob, destination_bucket, file_name)
+
+    # Delete the blob from the source bucket
+    source_blob.delete()
+
+    print(f"Moved {file_name} from {bucket_name} to {destination_bucket_name}")
